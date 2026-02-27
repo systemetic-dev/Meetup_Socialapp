b7f58b1326b1cfa778768a1f2715aee286ae2e33
DevVaishnav1369 <devvaishnav2007@gmail.com>
2026-02-27 22:35:02 +0530

    day 4 i understand lesson upto 14 very well, without confusion

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Post
 
 def hello(request):
-    return HttpResponse("Hy dev")
+    data = {
+        "name": "John ",
+        "age": 30,
+        "is_logged_in": True,
+        "fruits": ["Apple", "Banana", "Mango"]
+    }
+    return render(request,"blog/hello.html",data)
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {"posts": posts})
